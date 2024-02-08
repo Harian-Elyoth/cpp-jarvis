0e3965d8ac62f74d802fc7c6335f3f11277f596a
Exoalt <simon.rennotte@protonmail.com>
2024-02-08 20:00:09 +0100

    Adding message to api call

--- src/ApiHandler.cpp
+++ src/ApiHandler.cpp
@@ -31,10 +31,17 @@ std::string ApiHandler::call(std::string message) {
     CURL* curl;
     CURLcode res;
     ResponseData response_data;
+    struct curl_slist* headers = NULL;
+    std::string json_data = "{\"message\": \"" + message + "\"}";
+    const char* json_data_c_style = json_data.c_str();
+    //const char* json_data = "{\"name\": \"John Doe\", \"email\": \"john@example.com\"}";
+
 
     curl = curl_easy_init();
     if (curl) {
+        headers = curl_slist_append(headers, "Content-Type: application/json");
         curl_easy_setopt(curl, CURLOPT_URL, "https://google.com");
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, json_data_c_style);
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response_data);
         res = curl_easy_perform(curl);
